0dadbdaafe8ef753f64e52dd898ccdbf80cd1c6a
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2023-01-13 11:40:35 -0500

    add receiving code for rmq

--- hello-world/receive.js
+++ hello-world/receive.js
@@ -1,1 +1,33 @@
-console.log("im in");
+import amqp from "amqplib/callback_api.js";
+
+const queueName = "hello";
+
+// if we use object configuration, the username must be 'guest'
+// If we want to use something other then guest, we must create a new user with new permissions
+amqp.connect("amqp://localhost", (error0, connection) => {
+  if (error0) throw error0;
+  if (connection) console.log(connection, "\nWe are here");
+
+  connection.createChannel((error1, channel) => {
+    if (error1) throw error1;
+
+    // as a listener, we make sure the queue exists so that we dont crash if we start before the senders
+    channel.assertQueue(queueName, {
+      durable: false /* durable: true means queue will survive broker restart */,
+    });
+
+    channel.consume(
+      queueName,
+      async (message) => {
+        // Doesnt delay message retrieval
+        await new Promise((resolve, reject) => {
+          setTimeout(() => resolve(), 2000);
+        });
+        console.log("Recieved %s", message.content.toString());
+      },
+      {
+        noAck: true,
+      }
+    );
+  });
+});
--- hello-world/send.js
+++ hello-world/send.js
@@ -1,7 +1,8 @@
 import amqp from "amqplib/callback_api.js";
 
 const queueName = "hello";
-const message = "hello world: " + Math.ceil(Math.random() * 1000);
+const message = "hello world";
+const messagesToSend = 100;
 
 // if we use object configuration, the username must be 'guest'
 // If we want to use something other then guest, we must create a new user with new permissions
@@ -9,16 +10,23 @@ amqp.connect("amqp://localhost", (error0, connection) => {
   if (error0) throw error0;
   if (connection) console.log(connection, "\nWe're in");
 
-  connection.createChannel((error1, channel) => {
+  connection.createChannel(async (error1, channel) => {
     if (error1) throw error1;
 
     channel.assertQueue(queueName, {
       durable: false /* durable: true means queue will survive broker restart */,
     });
 
-    // queues message to be recieved by ./recieve.js
-    channel.sendToQueue(queueName, Buffer.from(message));
-    console.log("Sent %s", message);
+    for (let i = 0; i < messagesToSend; i++) {
+      const newMessage = message + ": " + Math.ceil(Math.random() * 1000);
+
+      // queues message to be recieved by ./recieve.js
+      channel.sendToQueue(queueName, Buffer.from(newMessage));
+      console.log("Sent %s", newMessage);
+      // await new Promise((resolve, reject) => {
+      //   setTimeout(resolve, 100);
+      // });
+    }
 
     setTimeout(function () {
       connection.close();
